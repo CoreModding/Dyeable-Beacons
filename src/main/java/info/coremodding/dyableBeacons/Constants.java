963e603c52fe2bbf649a9bc435e8c42bba4c3aa2
nxsupert <ethanmwebb@gmail.com>
2014-06-03 21:34:09 +0100

    Fixed Block Rendering

---
+++ src/main/java/info/coremodding/dyableBeacons/Constants.java
@@ -0,0 +1,8 @@
+package info.coremodding.dyableBeacons;
+
+public class Constants {
+	public static final String MOD_ID = "DyeableBeacons";
+	public static final String MOD_NAME = "Dyeable Beacons";
+	public static final String VERSION = "@VERSION";
+	
+}
--- src/main/java/info/coremodding/dyableBeacons/proxy/ClientProxy.java
+++ src/main/java/info/coremodding/dyableBeacons/proxy/ClientProxy.java
@@ -1,12 +1,22 @@
 package info.coremodding.dyableBeacons.proxy;
 
-import cpw.mods.fml.client.registry.ClientRegistry;
+import info.coremodding.dyableBeacons.blocks.BlockDyableBeacon;
+import info.coremodding.dyableBeacons.render.blocks.RenderDyableBeacons;
 import info.coremodding.dyableBeacons.render.tileEntity.TileEntityDyableBeaconRenderer;
 import info.coremodding.dyableBeacons.tileEntity.TileEntityDyableBeacon;
+import info.coremodding.dyableBeacons.DyableBeacons;
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
+import cpw.mods.fml.client.registry.RenderingRegistry;
 
 public class ClientProxy extends CommonProxy {
 	
 	public void registerRender() {
 		 ClientRegistry.bindTileEntitySpecialRenderer(TileEntityDyableBeacon.class, new TileEntityDyableBeaconRenderer());
+	
+		 BlockDyableBeacon.renderID = RenderingRegistry.getNextAvailableRenderId();
+		 ISimpleBlockRenderingHandler renderDyableBeacons = new RenderDyableBeacons();
+		 RenderingRegistry.registerBlockHandler(DyableBeacons.dyableBeacon().getRenderType(), renderDyableBeacons);
+	
 	}
 }
